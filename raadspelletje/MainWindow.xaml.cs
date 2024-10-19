0f6bce59a3d6f894b9552af502e403d20087e9a2
MericUcarPXL <meric.ucar@icloud.com>
2024-10-19 21:16:46 +0200

    Taak klaar

--- raadspelletje/MainWindow.xaml.cs
+++ raadspelletje/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,61 @@ namespace raadspelletje
         public MainWindow()
         {
             InitializeComponent();
+            random = new Random();
+            randomNumber = random.Next(1,100);
+        }
+
+        Random random;
+        int number1;
+        int randomNumber;
+        int aantalPogingen = 0;
+        bool geraden = false;
+
+
+
+        private void evaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            number1 = Convert.ToInt32(numberTextBox.Text);
+            if (randomNumber == number1)
+            {
+                output1TextBox.Text = ("Proficiat! U hebt het getal geraden!");
+                geraden = false;
+            }
+            else if(randomNumber < number1)
+            {
+                output1TextBox.Text = ("Raad lager!");
+                geraden = false;
+
+            }
+            else if (randomNumber > number1) {
+                output1TextBox.Text = ("Raad hoger!");
+                geraden = false;
+
+            }
+            if (!geraden) {
+
+                aantalPogingen++;
+                output2TextBox.Text = "Aantal keer geraden: " + $"{aantalPogingen}";
+            }
+            
+
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            output1TextBox.Clear();
+            output2TextBox.Clear();
+            numberTextBox.Clear();
+            aantalPogingen = 0;
+            random = new Random();
+            randomNumber = random.Next(1, 100);
+
+        }
+
+        private void endButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
